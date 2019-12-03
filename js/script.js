8d240f8ebdd5ee501443839179ef7d46952be9ef
ArielJans <arjapro@gmail.com>
2019-12-03 09:31:51 -0300

    Subida proyecto web

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// var nombre = "ariel";
+//var nombre = prompt("ingrese su nombre"); //metodo de entrada
+//var edad=37;
+//var comprobar = nombre == "ros" && edad > 18; // 
+//alert(comprobar);//otro metodo de salida
+// console.log(nombre) //metodo de salida
+
+//var num1 = parseInt(prompt("ingrese numero"));
+//var num2 = parseInt(prompt("ingrese otro numero"));
+//var resultado = num1 + num2;
+//console console.log(resultado);
+
+//var cadena1 = "hola ";
+//var cadena2 = "mundo";
+
+//var concatenacion = cadena1 + cadena2;
+
+//console.log(concatenacion);
+
+//var numero1 = parseInt("15");
+//var numero2 = parseInt("5");
+//var resultado = numero1 + numero2;
+//console.log(resultado); 
+//alert (resultado);
+
+//var edad = prompt("Ingrese su edad")
+//var colorPelo = prompt ("Ingrese el clor de pelo")
+//if (edad >= 18 && colorPelo == "negro"){
+//if (colorPelo == "negro") {
+    //alert ("Bienvenido")
+  //  document.getElementById("prueba").textContent = colorPelo;
+//}else{
+   // alert("Acceso no permitido")
+//}
+
+
